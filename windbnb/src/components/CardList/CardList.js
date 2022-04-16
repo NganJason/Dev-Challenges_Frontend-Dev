9aa1453fd489abecb3cc72cc8727eebc4619b223
Jason Ngan <jason.ngan@shopee.com>
2022-04-16 14:23:44 +0800

    add card list

--- windbnb/src/components/CardList/CardList.js
+++ windbnb/src/components/CardList/CardList.js
@@ -1,10 +1,24 @@
 import React from "react"
 
+import Card from "../Card/Card"
 function CardList() {
     return (
       <div>
-          
-      </div>);
+        <div className="cardlist__header">
+          <h1>Stays in Finland</h1>
+          <p>12+ stays</p>
+        </div>
+
+        <div className="cardlist">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    );
 }
 
 export default CardList
