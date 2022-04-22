99e0154884fba700a2b5c1e7fe7ada3b24500a03
Jason Ngan <jason.ngan@shopee.com>
2022-04-22 14:37:04 +0800

    edit overflow

--- todo-app/src/components/TodoList/style.js
+++ todo-app/src/components/TodoList/style.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export const StyledTodoList = styled.div`
-    display: flex;
-    flex-direction: column;
+  max-height: 50vh;
+  margin-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
